6ac617424c48e0eb5dd802fdffa6d282b1cb18db
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-04 16:48:35 +0300

    promises add

--- 1-app.js
+++ 1-app.js
@@ -1,7 +1,37 @@
-const _=require('lodash') 
+const { readFile, writeFile } = require("fs").promises;
+/* const util = require('util')
 
-const items = [1,[2,[3,[4]]]]
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile) */
 
-const newItems = _.flattenDeep(items)
 
-console.log(newItems)
+
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf-8");
+    const second = await readFile("./content/second.txt", "utf-8");
+    await writeFile('./content/result-mind-grenade.txt',`THIS IS AWESOME : ${first} ${second}`)
+    console.log(first, second);
+  } catch (err) {
+    console.log(err);
+  }
+};
+start();
+
+
+/* const getText = (path) => {
+    return new Promise((resolve, reject) => {
+      readFile("./content/first.txt", "utf-8", (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  };
+   */
+  /* getText("./content/first.txt")
+  .then((result) => console.log(result))
+  .catch((error) => console.log(error));
+ */
