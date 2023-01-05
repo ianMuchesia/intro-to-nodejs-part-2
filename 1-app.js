020fa24e75f3c3658c4c90b035bacb9c55d0a677
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-05 11:04:13 +0300

    add big data

--- 1-app.js
+++ 1-app.js
@@ -1,37 +1,7 @@
-const { readFile, writeFile } = require("fs").promises;
-/* const util = require('util')
+const {createReadStream} = require('fs')
 
-const readFilePromise = util.promisify(readFile)
-const writeFilePromise = util.promisify(writeFile) */
+const stream = createReadStream('./content/big.txt')
 
-
-
-const start = async () => {
-  try {
-    const first = await readFile("./content/first.txt", "utf-8");
-    const second = await readFile("./content/second.txt", "utf-8");
-    await writeFile('./content/result-mind-grenade.txt',`THIS IS AWESOME : ${first} ${second}`)
-    console.log(first, second);
-  } catch (err) {
-    console.log(err);
-  }
-};
-start();
-
-
-/* const getText = (path) => {
-    return new Promise((resolve, reject) => {
-      readFile("./content/first.txt", "utf-8", (error, data) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(data);
-        }
-      });
-    });
-  };
-   */
-  /* getText("./content/first.txt")
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
- */
+stream.on('data', (result)=>{
+  console.log(result)
+})
